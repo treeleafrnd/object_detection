b3d52fb540b9da3fd8da68ae96d7841e69ceef9b
immp-32 <immp3353@gmail.com>
2024-04-03 09:39:51 +0545

    updated

--- odservice/base_detector.py
+++ odservice/base_detector.py
@@ -1,6 +1,16 @@
 
 from typing import List
 
+from typing import List
+
 class BaseDetector:
-    def predict(self, image, thresh=0.5, detect_class=None):
+    def predict(self, image, detect_class_thresh=None) -> (List[tuple], List[float], List[str]):
+        """
+        detect_class_thresh= None  --- this parameter is a dictionary containing class as a key and thresh as value
+
+        -> (List[tuple], List[float], List[str]) --- it indicates that the method returns a tuple containing three list
+        -- list of bounding box(As tuples)
+        -- list of conf score as float
+        -- list of class names as strings
+        """
         raise NotImplementedError
--- odservice/yolov5_detector.py
+++ odservice/yolov5_detector.py
@@ -10,6 +10,14 @@ class YoloV5Detector(BaseDetector):
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
     def predict(self, image, detect_class_thresh = None):
+        """
+                detect_class_thresh= None  --- this parameter is a dictionary containing class as a key and thresh as value
+
+                -> (List[tuple], List[float], List[str]) --- it indicates that the method returns a tuple containing three list
+                -- list of bounding box(As tuples)
+                -- list of conf score as float
+                -- list of class names as strings
+                """
 
         results = self.model(image)
         # converting predicted image into pandas dataframe
